d6ac0feed0ce73298608ef8aef4452a7d0941112
Alexis Mauricio Gunsett <mau_gunsett@hotmail.com>
2024-01-18 11:44:29 -0300

    agrego register view

--- src/Router.jsx
+++ src/Router.jsx
@@ -5,6 +5,7 @@ import AdminView from './views/AdminView';
 import DetailView from './views/DetailView';
 import HomeView from './views/HomeView';
 import LoginView from './views/LoginView';
+import RegisterView from './views/RegisterView';
 import ErrorView from './views/ErrorView';
 
 import Navbar from './components/Common/Navbar';
@@ -22,6 +23,7 @@ const Router = ()=> {
       <Route path='/' element={<HomeView />} />
       <Route path='/detalle' element={<DetailView />} />
       <Route path='/login' element={<LoginView />} />
+      <Route path='/register' element={<RegisterView />} />
       <Route path='/admin' element={<AdminView />} />
       <Route path='*' element={<ErrorView />} />
   </Routes>
